4d46001481102a16bcf752caba35961b108a78c2
djemai erraid <nm_djemai@esi.dz>
2024-09-16 10:52:01 +0100

    just fixing the app rendering urls

--- app.js
+++ app.js
@@ -15,5 +15,7 @@ console.log(`Server running at http://localhost:${port}`);
 app.get('/', (req, res) => { res.render('home')})
 app.get('/signup', (req, res) => {res.render('signup')});
 app.get('/login', (req, res) => {res.render('Login')});
+app.get('/add_post', (req, res) => {res.render('add_post')});
+app.get('/main', (req, res) => {res.render('main')});
 app.post('/signup', (req, res) => {});
 app.post('/login', (req, res) => {});
